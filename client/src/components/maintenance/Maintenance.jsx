15e42c858d58c376d4c99617c3c7c3b61f202d6c
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-12-07 16:53:44 +0200

    project

--- client/src/components/maintenance/Maintenance.jsx
+++ client/src/components/maintenance/Maintenance.jsx
@@ -1,12 +1,9 @@
-import { Link } from "react-router-dom";
 import Hero from "./Hero.jsx";
 import HeaderB from "./HeaderB.jsx";
 import MaintenanceForm from "./MaintenanceForm.jsx";
 import Banners from "./Banners.jsx";
 
 export default function Maintenance() {
-  
-
   return (
     <div>
       <Hero/>
