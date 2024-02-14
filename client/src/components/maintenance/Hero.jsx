30787bc6b1ab6bca333d98382f6ea307c9ce6619
Mustafa Dzhindzhi <dzindzimustafa@gmail.com>
2024-02-14 20:59:27 +0200

    project

--- client/src/components/maintenance/Hero.jsx
+++ client/src/components/maintenance/Hero.jsx
@@ -7,9 +7,11 @@ export default function Hero({ onTakeOfferClick }) {
   
     return (
       <section id="hero">
+        <div className="hero-background">
         <h4>Feel free to trust US!</h4>
         <h2>Brand support for over 5 years now from €199</h2>
         <button onClick={handleTakeOfferClick}>Book a maintenance</button>
+        </div>
       </section>
     );
   }
